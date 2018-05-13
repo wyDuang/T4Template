28d4d9172ce4431cd6f545bee7c29268d4f280f9
wyDuang <1014558384@qq.com>
2018-05-13 14:45:05 +0800

    更新了 Dal 和 增加了Bll

--- T4Dal/ArticleDal.cs
+++ T4Dal/ArticleDal.cs
@@ -9,21 +9,21 @@ using System.Linq;
 
 namespace ExcessChip.DAL
 {
-	/// <summary>
-	/// -Article 
-	/// </summary>
 	public class ArticleDal
 	{ 
-		private static readonly string connString = ConfigHelper.GetConnectionString("connStr");
+		private static readonly string connStr = ConfigHelper.GetConnectionString("ExcessChipStr");
 		private SqlConnection _conn;
-		public SqlConnection Conn {
-			get {
-				_conn = new SqlConnection(connString);
+		public SqlConnection Conn
+		{
+			get
+			{
+				_conn = new SqlConnection(connStr);
 				_conn.Open();
 				return _conn;
 			}
 		}
 
+		#region 默认方法
 		/// <summary>
 		/// 增加一条数据
 		/// </summary>
@@ -32,7 +32,7 @@ namespace ExcessChip.DAL
 		{
 			using (Conn)
 			{
-				string sql=@"insert into Article (Title, ArticleTypeId, ApproveStatus, ComeFrom, ClickCount, Avatar, IsTop, Content, Summary, KeyName, LocationType, Tag, CreateDate, UpdateDate, UserId, OperatorId) values(@Title, @ArticleTypeId, @ApproveStatus, @ComeFrom, @ClickCount, @Avatar, @IsTop, @Content, @Summary, @KeyName, @LocationType, @Tag, @CreateDate, @UpdateDate, @UserId, @OperatorId); SELECT @@IDENTITY";
+				string sql = @"insert into Article (Title, ArticleTypeId, ApproveStatus, ComeFrom, ClickCount, Avatar, IsTop, Content, Summary, KeyName, LocationType, Tag, CreateDate, UpdateDate, UserId, OperatorId) values(@Title, @ArticleTypeId, @ApproveStatus, @ComeFrom, @ClickCount, @Avatar, @IsTop, @Content, @Summary, @KeyName, @LocationType, @Tag, @CreateDate, @UpdateDate, @UserId, @OperatorId); SELECT @@IDENTITY";
 				return Conn.ExecuteScalar(sql.ToString(), model);
 			}
 		}
@@ -45,7 +45,7 @@ namespace ExcessChip.DAL
 		{
 			using (Conn)
 			{
-				string sql=@"update set Article Title=@Title,ArticleTypeId=@ArticleTypeId,ApproveStatus=@ApproveStatus,ComeFrom=@ComeFrom,ClickCount=@ClickCount,Avatar=@Avatar,IsTop=@IsTop,Content=@Content,Summary=@Summary,KeyName=@KeyName,LocationType=@LocationType,Tag=@Tag,CreateDate=@CreateDate,UpdateDate=@UpdateDate,UserId=@UserId,OperatorId=@OperatorId where id=@id";
+				string sql = @"update set Article Title=@Title, ArticleTypeId = @ArticleTypeId, ApproveStatus = @ApproveStatus, ComeFrom = @ComeFrom, ClickCount = @ClickCount, Avatar = @Avatar, IsTop = @IsTop, Content = @Content, Summary = @Summary, KeyName = @KeyName, LocationType = @LocationType, Tag = @Tag, CreateDate = @CreateDate, UpdateDate = @UpdateDate, UserId = @UserId, OperatorId = @OperatorId where id = @id";
 				return Conn.Execute(sql.ToString(), model);
 			}
 		}
@@ -58,8 +58,8 @@ namespace ExcessChip.DAL
 		{
 			using (Conn)
 			{
-				string sql=@"delete from Article where Id = @Id";
-				return Conn.Execute(sql.ToString());
+				string sql = @"delete from Article where Id = @Id";
+				return Conn.Execute(sql.ToString(), id);
 			}
 		}
 
@@ -71,36 +71,40 @@ namespace ExcessChip.DAL
 		{
 			using (Conn)
 			{
-				string sql="select * from Article where Id=@Id";
+				string sql="select * from Article where Id = @Id";
 				return Conn.Query<Article>(sql, new { Id = id }).SingleOrDefault();
 			}
 		}
 
 		/// <summary>
-		/// 获取实体List
+		/// 获取实体集合
 		/// </summary>
 		/// <returns></returns>
-		public List<Article> GetArticleList(int top, string where = null, string order = null)
+		public IEnumerable<Article> GetArticleList()
 		{
 			using (Conn)
 			{
 				string sql="select * from Article";
-				return Conn.Query<Article>(sql).ToList();
+				return Conn.Query<Article>(sql);
 			}
 		}
 
 		/// <summary>
-		/// 检测字段值是否存在
+		/// 检测某字段的值是否存在
 		/// </summary>
 		/// <returns></returns>
-		public bool IsColumnExist(string key,string val)
+		public int IsColumnExist(string key, string value)
 		{
 			using (Conn)
 			{
-				string sql="select count(*) from Article where {0}='{1}' ";
-				return Conn.QuerySingle<int>(string.Format(sql, key, val)) > 0;
+				string sql="select count(*) from Article where {0} = '{1}'";
+				return Conn.QuerySingle<int>(string.Format(sql, key, value));
 			}
 		}
+		#endregion
 
+		#region 自定义方法
+
+		#endregion
 	}
 }
